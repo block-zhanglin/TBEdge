704a504368e58d7b85eddd5b2ced1cb2ddcae823
block-zhanglin <140621233+block-zhanglin@users.noreply.github.com>
2023-09-09 14:48:53 +0800

    Update CoreConfiguration.java

--- BlockEs - 线程修改版本/blockchain-core/src/main/java/com.zl.blockchain.core/CoreConfiguration.java
+++ BlockEs - 线程修改版本/blockchain-core/src/main/java/com.zl.blockchain.core/CoreConfiguration.java
@@ -2,9 +2,6 @@ package com.zl.blockchain.core;
 
 /**
  * Core配置: BlockchainCore的配置。
- * 该类对BlockchainCore模块的配置进行统一管理。
- * 在这里可以持久化配置信息。
- * 理论上，BlockchainCore模块需要的任何配置，都可以从该对象获取。
  *
  */
 public abstract class CoreConfiguration {
@@ -44,10 +41,6 @@ public abstract class CoreConfiguration {
 
     /**
      * 矿工挖矿时间周期
-     * 矿工在组装好矿之后，系统会分配矿工一个确定的时间(这个时间就叫矿工挖矿时间周期)来让矿工进行挖矿，
-     * 在一个挖矿周期内，矿工并不一定能挖到矿，一个周期过去之后，矿工会停一停，歇一歇，然后重新组装矿再继续去挖矿。
-     * 为什么要重新组装矿？因为如果矿一直不变，在上次矿形成之后，用户新提交的未确认交易，就没有办法打包进矿了
-     * ，交易会很延迟才能得到确认。-------最小挖矿周期
      */
 
     public abstract long getMinerMineTimeInterval();
