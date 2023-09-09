404488f41dd85aed8515fe9dce1b8828edc0d12f
block-zhanglin <140621233+block-zhanglin@users.noreply.github.com>
2023-09-09 14:53:36 +0800

    Update BlockchainNetCore.java

--- BlockEs - 线程修改版本/blockchain-netcore/src/main/java/com/zl/blockchain/netcore/BlockchainNetCore.java
+++ BlockEs - 线程修改版本/blockchain-netcore/src/main/java/com/zl/blockchain/netcore/BlockchainNetCore.java
@@ -12,36 +12,6 @@ import com.zl.blockchain.netcore.service.NodeService;
  *  区块链网络版核心底层依赖单机版区块链核心BlockchainCore，在单机版区块链系统的基础上
  * ，新增了网络功能：自动的在整个区块链网络中寻找/发布：节点、区块、交易。
  *
- * 在启动时，它通过[种子节点初始化器]，将种子节点加入到自己已知的节点列表，
- * 通过[节点搜寻器]搜索区块链网络中的节点，
- * 通过[节点广播器]将自己的存在告诉其它节点，
- * 通过[区块链高度搜索器]搜索已知节点的高度，
- * 通过[区块搜寻器]查找最新的区块，
- * 通过[区块链高度广播器]将自己的高度告诉其它节点，
- * 通过[区块广播器]广播自己最新的区块，
- * 每个[区块链网络版核心]都会做出上述操作，从而相互之间互联起来，共同协作，构成了区块链网络。
- *
- * 区块链网络版核心系统，由以下几部分组成：
- * 单机版[没有网络交互版本]区块链核心
- * @see BlockchainCore
- * 种子节点初始化器
- * @see SeedNodeInitializer
- * 节点搜寻器
- * @see NodeSearcher
- * 节点广播器
- * @see NodeBroadcaster
- * 区块链高度搜索器
- * @see BlockchainHeightSearcher
- * 区块链高度广播器
- * @see BlockchainHeightBroadcaster
- * 区块搜寻器
- * @see BlockSearcher
- * 区块广播器
- * @see BlockBroadcaster
- * 未确认交易搜索器
- * @see UnconfirmedTransactionsSearcher
- *
- *
  */
 public class BlockchainNetCore {
 
